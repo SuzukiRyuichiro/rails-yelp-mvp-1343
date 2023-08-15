86a6b759e060133f540fab3968059baf483aef97
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2023-08-16 00:24:40 +0100

    g review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_15_232246) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_232341) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -20,4 +20,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_232246) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "restaurant_id", null: false
+    t.integer "rating"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end
